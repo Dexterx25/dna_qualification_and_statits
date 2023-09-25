280e6257e230c80a17ea2c29df814a4f60821211
@chicoOmicrom <jblanco@lya-electronic.com>
2023-09-24 23:59:44 -0500

    feat: updates

--- src/utils/dayjsFormatter/dayjs.module.ts
+++
@@ -1,8 +0,0 @@
-import { Module } from '@nestjs/common';
-import { DayJsService } from './dayjs.service';
-
-@Module({
-  exports: [DayJsService],
-  providers: [DayJsService],
-})
-export class DayJsModule {}
